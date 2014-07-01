c45d212cfc59a5f46177947dd6af44939b2d94ee
stefan <dev@2904.cc>
2014-07-01 04:59:11 +0200

    optimized gmail script

--- gmail/message_date_logger.py
+++ gmail/message_date_logger.py
@@ -8,6 +8,7 @@
 # check out https://developers.google.com/gmail/api/quickstart/quickstart-python
 # and https://console.developers.google.com
 
+import sys
 import httplib2
 import dateutil.parser
 
@@ -20,15 +21,25 @@ from oauth2client.tools import run
 def GetMessage(service, user_id, msg_id):
   try:
     message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+    headerFound = False
+
     for header in message['payload']['headers']:
-    	if header['name'] == 'Date': print dateutil.parser.parse(header['value']).replace(day=15, hour=0, minute=0, second=0, microsecond=0).strftime('%s')
+      if header['name'] == 'Date': 
+        headerFound = True
+        print dateutil.parser.parse(header['value']).replace(day=15, hour=0, minute=0, second=0, microsecond=0).strftime('%s')
+
+    if not headerFound:
+      print >>sys.stderr, 'message', msg_id,  'without date (a hangout?)'
+
   except errors.HttpError, error:
-    print 'An error occurred: %s' % error
+    print 'An error occurred: %s (%s)' % error, header['value']
+  except ValueError, error:
+    print 'An ValueError occurred: %s' % error
 
 def parseMessagePage(messages, service):
-	msgs = []
-	for m in messages:
-		GetMessage(service, 'me', m['id'])
+  msgs = []
+  for m in messages:
+    GetMessage(service, 'me', m['id'])
 
 # Path to the client_secret.json file downloaded from the Developer Console
 CLIENT_SECRET_FILE = 'client_secret.json'
@@ -54,28 +65,31 @@ http = credentials.authorize(http)
 # Build the Gmail service from discovery
 service = build('gmail', 'v1', http=http)
 
-onlyFirstPage = False # for testing
+onlyFirstPage = False  # for testing
+startAtMessage = 0 # if script fails => guess
+startAtPage = None     # if script fails => remember last page token
+
 counter = 0
 try:
-    response = service.users().messages().list(userId='me').execute()
-    #messages = []
-    #msgs = []
+    if startAtPage == None: response = service.users().messages().list(userId='me').execute()
+    else: response = service.users().messages().list(userId='me', pageToken=startAtPage).execute()
+
     counter = counter + 1
-    if 'messages' in response:
-      #messages.extend(response['messages'])
-      #msgs.extend(parseMessagePage(response['messages'], service))
+
+    if startAtPage == 0 and 'messages' in response:
       parseMessagePage(response['messages'], service)
 
     while not onlyFirstPage and 'nextPageToken' in response:
-      counter = counter + 1
+      counter = counter + len(response['messages'])
       #if counter % 10 == 0: gc.collect()
-      #print "reading page", counter
 
       page_token = response['nextPageToken']
-      response = service.users().messages().list(userId='me',
-                                         pageToken=page_token).execute()
+      response = service.users().messages().list(userId='me', pageToken=page_token).execute()
       #messages.extend(response['messages'])
-      parseMessagePage(response['messages'], service)
+      if counter > startAtMessage: 
+        parseMessagePage(response['messages'], service)
+        if 'nextPageToken' in response: print >>sys.stderr, 'nextPageToken:', response['nextPageToken'], 'at', counter
+
 except errors.HttpError, error:
     print 'An error occurred: %s' % error
 
